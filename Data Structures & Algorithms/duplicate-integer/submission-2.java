a89c7ec3d05e4cd3360748abcbaae99a8173a0fe
Hasth Karkera <hasthpoojary@gmail.com>
2026-06-17 23:32:35 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int flag=0;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                flag=1;
+            }
+        }
+        if(flag==1)
+        return true;
+        else
+        return false;
+        
+    }
+    
+}
