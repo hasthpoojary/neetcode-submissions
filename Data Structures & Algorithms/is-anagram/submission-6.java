3fad91cf80e80ddb420277432667a698320ee331
Hasth Karkera <hasthpoojary@gmail.com>
2026-07-29 07:45:14 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+        char[] charss = t.toCharArray();
+        Arrays.sort(charss);
+        String x = new String(chars);
+        String y= new String(charss);
+
+        if(x.equals(y))
+            return true;
+            else
+            return false;
+    }
+}
